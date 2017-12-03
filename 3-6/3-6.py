aa6f64c3a1beb82a953d748714a929e016b182cb
XILYA46X\Ilya Anichkin <xpapapapokerfacex@gmai.com>
2017-12-03 13:32:00 +0300

    3-6 доработка

--- 3-6/3-6.py
+++ 3-6/3-6.py
@@ -1,35 +1,36 @@
 class Animal():
-    def body(self, value):
-        self.value = 1
-
-    def head(self, value):
-        self.value = 1
-
-    def paws(self, value):
-        self.value = 4
-
-    def tail(self, value):
-        self.value = 1
+    def __init__(self, body=1, head=1, paws=4, tail=1, weight=50, height=50):
+        self.body = body
+        self.head = head
+        self.paws = paws
+        self.tail = tail
+        self.weight = weight
+        self.height = height
 
 
 class Livestock(Animal):
-    def nose(self, value):
-        self.value = 1
-
-    def hooves(self, value):
-        self.value = 4
-
+    def __init__(self, nose=1, body=1, head=1, paws=4, tail=1, weight=50, height=50):
+        self.nose = nose
+        self.hooves = paws
+        super().__init__(body, head, paws, tail, weight, height)
 
 
 class HomeBirds(Animal):
-    def beak(self, value):
-        self.value = 1
+    def __init__(self, wingspan=50, beak=1, wings=2, feathers='Перья', body=1, head=1, paws=2, tail=1, weight=50,
+                 height=50):
+        self.wingspan = wingspan
+        self.beak = beak
+        self.wings = wings
+        self.feathers = feathers
+        super().__init__(body, head, paws, tail, weight, height)
 
-    def feathers(self):
-        print('Перья')
 
-    def wings(self, value):
-        self.value = 4
+cow = Livestock(weight=1000, height=140)
+goat = Livestock(weight=100, height=50)
+sheep = Livestock(weight=80, height=70)
+pig = Livestock(weight=90, height=80)
 
-
+duck = HomeBirds(wingspan=80, weight=2)
+chicken = HomeBirds(wingspan=40, weight=1, height=40)
+goose = HomeBirds(wingspan=160, weight=3, height=90)
 
