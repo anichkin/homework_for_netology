c31a68d5c8d56b720df85a70728fb2cf74b4e320
XILYA46X\Ilya Anichkin <xpapapapokerfacex@gmai.com>
2018-01-11 22:39:58 +0300

    diploma

--- Diploma/Diploma.py
+++ Diploma/Diploma.py
@@ -1,12 +1,5 @@
 import requests
-import time
-
-TOKEN = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
-VERSION = '5.69'
-
-
-#
-
+import json
 
 
 def get_ids(ids, VERSION):
@@ -17,16 +10,13 @@ def get_ids(ids, VERSION):
 
     response = requests.get('https://api.vk.com/method/users.get', params)
     response_json = response.json()
-    id = response_json['response'][0]['id']
-    return id
+    user_id = response_json['response'][0]['id']
+    return user_id
 
 
-print(get_ids('papapapokerface', VERSION))
-
-
-def get_friends_list(friend_id, VERSION):
+def get_friends_list(user_id, VERSION):
     friend_params = {
-        'user_id': friend_id,
+        'user_id': user_id,
         'v': VERSION
     }
 
@@ -37,10 +27,10 @@ def get_friends_list(friend_id, VERSION):
         return friend_list
 
 
-def get_group_list(friend_id, VERSION):
+def get_group_list(user_id, VERSION, TOKEN):
     group_params = {
         'access_token': TOKEN,
-        'user_id': friend_id,
+        'user_id': user_id,
         'count': 1000,
         'v': VERSION
     }
@@ -51,35 +41,8 @@ def get_group_list(friend_id, VERSION):
     return group_list
 
 
-def unique_group(friend_list, group, VERSION):
-    for friend in friend_list:
-        params = {
-            'group_id': group,
-            'user_id': 1,
-            'user_ids': friend,
-            'extended': 1,
-            'v': VERSION
-        }
-        response = requests.get('https://api.vk.com/method/groups.isMember', params)
-        response_json = response.json()
-        is_member = response_json['response'][0]['member']
-        if is_member:
-            print('группа', group, 'не уникальна')
-            break
-        else:
-            print('.', end=' ')
-    if not is_member:
-        print('группа уникальна!!!')
-        return group
 
-
-friend_list = get_friends_list(get_ids('papapapokerface', VERSION), VERSION)
-group_list = get_group_list('15590964', VERSION)
-print(group_list)
-print(str(friend_list))
-
-
-def response(friends_list):
+def response(friends_list, group, VERSION):
     params = {
         'group_id': group,
         'user_ids': friends_list,
@@ -98,73 +61,67 @@ def response(friends_list):
             return group
 
 
-def unique_group2(friend_list, group, VERSION):
+def unique_group(friend_list, group, VERSION):
     if len(friend_list) > 300:
         split_friend_list = [friend_list[d:d + 300] for d in range(0, len(friend_list), 300)]
         for list in split_friend_list:
             t = 0
             friends_list = ((str(list)).replace('[', "'")).replace(']', "'")
-            result = response(friends_list)
+            result = response(friends_list, group, VERSION)
             if not result:
                 t == 0
+                print()
                 print('группа', group, 'не уникальна')
                 break
             else:
                 t += 1
         if t != 0:
-            print('группа уникальна', group)
+            print()
+            print('группа УНИКАЛЬНА', group)
             return group
 
 
     else:
         friends_list = ((str(friend_list)).replace('[', "'")).replace(']', "'")
-        result = response(friends_list)
+        result = response(friends_list, group, VERSION)
         if not result:
+            print()
             print('группа', group, 'не уникальна')
         else:
-            print('группа уникальна', group)
+            print()
+            print('группа УНИКАЛЬНА', group)
             return group
 
-listtt = []
-for group in group_list:
-    if unique_group2(friend_list, group, VERSION):
-        listtt.append(unique_group2(friend_list, group, VERSION))
-print(listtt)
-listt = ((str(listtt)).replace('[', "'")).replace(']', "'")
-params = {
-        'group_ids': listt,
-        'fields': 'members_count',
-        'v': VERSION
-    }
-response = requests.get('https://api.vk.com/method/groups.getById', params)
-response_json = response.json()
-print(response_json)
+
+def group_json(group_list, friend_list, VERSION):
+    group_list_for_json = []
+    for group in group_list:
+        if unique_group(friend_list, group, VERSION):
+            group_list_for_json.append(unique_group(friend_list, group, VERSION))
+    group_list_for_json = ((str(group_list_for_json)).replace('[', "'")).replace(']', "'")
+    params = {
+            'group_ids': group_list_for_json,
+            'fields': 'members_count',
+            'v': VERSION
+        }
+    response = requests.get('https://api.vk.com/method/groups.getById', params)
+    response_json = response.json()
+    groups = response_json['response']
+    print(groups)
+    with open('group.json', 'w', encoding='utf-8') as f:
+        json.dump(groups, f, indent=2, ensure_ascii=False)
+
+def program():
+    TOKEN = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
+    VERSION = '5.69'
+    user_id = input('Введите имя пользователя или его id')
+    user_id = get_ids(user_id, VERSION)
+    print(get_ids(user_id, VERSION))
+    friend_list = get_friends_list(user_id, VERSION)
+    print(friend_list)
+    group_list = get_group_list(user_id, VERSION, TOKEN)
+    print(group_list)
+    group_json(group_list, friend_list, VERSION)
 
 
-
-
-
-
-
-
-
-
-fried_list = get_friends_list(get_ids('papapapokerface', VERSION), VERSION)
-group_list = get_group_list('15590964', VERSION)
-print(str(fried_list))
-frienssss = ((str(fried_list)).replace('[', "'")).replace(']', "'")
-print(frienssss)
-
-# print(group_list)
-# for group in group_list:
-#     unique_group(fried_list, group, VERSION)
-
-params = {
-    'group_id': 31480508,
-    'user_ids': frienssss,
-    'extended': 0,
-    'v': VERSION
-}
-response = requests.get('https://api.vk.com/method/groups.isMember', params)
-response_json = response.json()
-print(response_json['response'])
+program()
