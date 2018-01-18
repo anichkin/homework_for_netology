52a33d05da04368e56ff133b18a44337ff8b9522
XILYA46X\Ilya Anichkin <xpapapapokerfacex@gmai.com>
2018-01-18 22:30:01 +0300

    diploma

--- Diploma/Diploma.py
+++ Diploma/Diploma.py
@@ -1,19 +1,29 @@
 import requests
 import json
-from urllib.parse import urlencode, urljoin
+import time
+from urllib.parse import urljoin
 
 
 def do_request(params, method):
-    URL = 'https://api.vk.com/method/'
+    url = 'https://api.vk.com/method/'
     print('.', end=' ')
-    response = requests.get(urljoin(URL, method), params)
+    response = requests.get(urljoin(url, method), params)
     response.raise_for_status()
     response_json = response.json()
     if 'error' in response_json:
-        print(response_json)
-    if 'response' in response_json:
-        return response_json['response']
-
+        if response_json['error']['error_code'] == 6:
+            print(response_json['error']['error_msg'])
+            time.sleep(1)
+            do_request(params, method)
+        if response_json['error']['error_code'] == 15:
+            print(response_json['error']['error_msg'])
+            return 0
+        if response_json['error']['error_code'] == 113:
+            print(response_json['error']['error_msg'])
+            return 0
+        print(response_json['error']['error_msg'])
+        print(method)
+    return response_json['response']
 
 
 def get_ids(ids, version):
@@ -21,8 +31,10 @@ def get_ids(ids, version):
         'user_ids': ids,
         'v': version
     }
-    user_id = do_request(params,'users.get')[0]['id']
-    return user_id
+    user_id = do_request(params, 'users.get')
+    if not user_id:
+        return 0
+    return user_id[0]['id']
 
 
 def get_friends_list(user_id, version):
@@ -30,9 +42,11 @@ def get_friends_list(user_id, version):
         'user_id': user_id,
         'v': version
     }
-    friends_list = do_request(params, 'friends.get')['items']
-    return friends_list
-
+    friends_list = do_request(params, 'friends.get')
+    if not friends_list:
+        print('Доступ к пользователю запрещен')
+        program()
+    return friends_list['items']
 
 
 def get_group_list(user_id, version, token):
@@ -42,12 +56,11 @@ def get_group_list(user_id, version, token):
         'count': 1000,
         'v': version
     }
+    group_list = do_request(params, 'groups.get')
+    return group_list['items']
 
-    group_list = do_request(params, 'groups.get')['items']
-    return group_list
 
-
-def response(friends_list, group, version):
+def is_member(friends_list, group, version):
     params = {
         'group_id': group,
         'user_ids': friends_list,
@@ -55,102 +68,84 @@ def response(friends_list, group, version):
         'v': version
     }
     is_member = do_request(params, 'groups.isMember')
-    for member in is_member:
-        if member['member']:
-            return 0
-        else:
-            print('.', end=' ')
-    if not member['member']:
-        return group
+    return is_member
 
 
-def unique_group(friend_list, group, version, test=0, chunk_size=300):
+def unique_group(friend_list, group, version, chunk_size=300):
     if len(friend_list) > chunk_size:
         split_friend_list = [friend_list[d:d + chunk_size] for d in range(0, len(friend_list), chunk_size)]
         for friend_list in split_friend_list:
             friends_list = ', '.join(str(friend) for friend in friend_list)
-            result = response(friends_list, group, version)
+            result = is_member(friends_list, group, version)
             if not result:
-                print()
-                print('группа', group, 'не уникальна')
-                return False
-            else:
-                test += 1
-                continue
-        if test != 0:
-            print()
-            print('группа', group, 'УНИКАЛЬНА')
-            return group
+                return 0
+            for member in result:
+                if member['member']:
+                    print(('группа {} не уникальна').format(group))
+                    return 0
+        print(('группа {} УНИКАЛЬНА').format(group))
+        return group
 
-    else:
-        friends_list = ', '.join(str(friend) for friend in friend_list)
-        result = response(friends_list, group, version)
-        if not result:
-            print()
-            print('группа', group, 'не уникальна')
-        else:
-            print()
-            print('группа', group, 'УНИКАЛЬНА')
-            return group
+    friends_list = ', '.join(str(friend) for friend in friend_list)
+    result = is_member(friends_list, group, version)
+    if not result:
+        return 0
+    for member in result:
+        if member['member']:
+            print(('группа {} не уникальна').format(group))
+            return 0
+    print(('группа {} УНИКАЛЬНА').format(group))
+    return group
 
 
-def group_json(group_list, friend_list, version):
+def group_json(group_list, friend_list, version, json_group=[]):
     group_list_for_json = []
     for group in group_list:
         if unique_group(friend_list, group, version):
             group_list_for_json.append(group)
-        else:
-            continue
+    if len(group_list_for_json) == 0:
+        print('У этого пользователя нет уникальных групп')
+        program()
     group_list_for_json = ', '.join(str(group) for group in group_list_for_json)
     params = {
         'group_ids': group_list_for_json,
         'fields': 'members_count',
+        'extended': 1,
         'v': version
     }
-    response = requests.get('https://api.vk.com/method/groups.getById', params)
-    response_json = response.json()
-    groups = response_json['response']
-    with open('group.json', 'w') as f:
-        json.dump(groups, f, indent=2, ensure_ascii=False)
+    groups = do_request(params, 'groups.getById')
+    if not groups:
+        return 'Нет таких групп'
+    for i, group in enumerate(groups):
+        group_param = {}
+        group_param['name'] = group['name']
+        group_param['id'] = group['id']
+        group_param['members_count'] = group['members_count']
+        json_group.append(group_param)
+    with open('group.json', 'w', encoding='UTF-8') as f:
+        json.dump(json_group, f, indent=2, ensure_ascii=False)
+        print('список уникальных групп в файле group.json')
 
 
 def program():
-    token = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
-    version = '5.69'
+    file = open('config.json', 'r')
+    config = json.load(file)
+    token = config['token']
+    version = config['version']
     user_id = input('Введите имя пользователя или его id: ')
     user_id = get_ids(user_id, version)
+    if not user_id:
+        print('нет такого пользователя, введите заново')
+        program()
     friend_list = get_friends_list(user_id, version)
+    if len(friend_list) == 0:
+        print('У этого пользователя нет друзей')
+        program()
     group_list = get_group_list(user_id, version, token)
-    group_json(group_list, friend_list, version)
-    print('список уникальных групп в файле group.json')
-
-
-
-# def do_request(params, method):
-#     URL = 'https://api.vk.com/method/'
-#     response = requests.get(urljoin(URL, method), params)
-#     response.raise_for_status()
-#     response_json = response.json()
-#     if 'error' in response_json:
-#         print(response_json)
-#     if 'response' in response_json:
-#         return response_json['response']
-
-
-
-
-
-# params = {
-#         'user_id': 15590964,
-#         'v': '5.69'
-#     }
-# do_request(params,'friends.get')
-
+    if len(group_list) == 0:
+        print('у этого пользователя нет групп')
+        program()
+    group_list_for_json = group_json(group_list, friend_list, version)
 
 
 program()
-# config = open('config.py', 'r')
-# token = config.readline()
-# version = config.readline()
-# print(token)
-# print(version)
